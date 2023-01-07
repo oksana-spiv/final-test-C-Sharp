b556c5d5a492f498af8094dac9bd25963a31f8b8
osa-home <osa@mail.com>
2023-01-08 00:22:27 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string[] array1 = new string[] {"1234", "12567", "2", "-2", ":)", "Russia",  "hello", "world", "computer", "oks"};
+string[] array2 = new string[array1.Length];
+Console.Write ("Задан следующий массив:  ");
+Console.Write ("[");
+WriteArray (array1);
+Console.Write ("]");
+Console.WriteLine ();
+Console.Write ("Из него мы выбрали элементы длинной не более 3 символов:  ");
+SecondArrayWithIF(array1, array2);
+WriteArray (array2);
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void WriteArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
